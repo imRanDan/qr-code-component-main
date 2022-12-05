458d5457d5acf8750937361361c6cb5eb62bff17
Danyal Imran <dan.imran97@gmail.com>
2022-12-05 13:38:02 -0500

    finished project :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
-    extend: {},
+    extend: {
+      colors: {
+        white: 'hsl(0, 0%, 100%)',
+        lightGray: 'hsl(212, 45%, 89%)',
+        grayishBlue: 'hsl(220, 15%, 55%)',
+        darkBlue: 'hsl(218, 44%, 22%)',
+      }
+    },
   },
   plugins: [],
 }
